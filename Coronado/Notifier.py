5380d30081c75926ae14a8e25c618d301eddedff
Mukul Majmudar <mukul@curecompanion.com>
2014-07-14 18:59:27 +0000

    Added Apple Push Notification Service notifier.

---
+++ Coronado/Notifier.py
@@ -0,0 +1,126 @@
+import socket
+import ssl
+import struct
+import binascii
+from random import random
+import json
+import time
+from datetime import datetime
+import logging
+
+import tornado.iostream
+from tornado.concurrent import Future
+from tornado.ioloop import IOLoop
+
+logger = logging.getLogger(__name__)
+
+class PayloadTooLong(Exception):
+    pass
+
+class APNsNotifier(object):
+
+    def __init__(self, apnsArgs, sslArgs):
+        self._apnsArgs = apnsArgs
+        self._sslArgs = sslArgs
+        self._connected = False
+
+
+    def connect(self):
+        self._connectFuture = Future()
+
+        # Create a non-blocking SSL socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setblocking(0)
+
+        # Make tornado iostream
+        sslOptions = \
+        {
+            'certfile': self._apnsArgs['certFilePath'],
+            'ca_certs': self._sslArgs['caCertsFilePath'],
+            'cert_reqs': ssl.CERT_REQUIRED
+        }
+        self._iostream = tornado.iostream.SSLIOStream(s, ssl_options=sslOptions)
+
+        def onConnected():
+            logger.info('APNs connected')
+            self._connectFuture.set_result(None)
+            self._connected = True
+
+        # Connect
+        self._iostream.connect((self._apnsArgs['host'], self._apnsArgs['port']),
+                onConnected)
+
+        # Perform read for error packets
+        self._iostream.read_bytes(6, self._onError)
+
+        return self._connectFuture
+
+
+    def send(self, notification):
+        def _send():
+            # Make sure payload is not too long
+            payload = json.dumps(notification['payload'])
+            if len(payload) > 256:
+                raise PayloadTooLong()
+
+            # Pack device token item
+            tokenItem = struct.pack('!BH32s', 1, 32, 
+                    binascii.unhexlify(notification['token']))
+
+            # Pack payload item
+            payloadItem = struct.pack('!BH' + str(len(payload)) + 's',
+                    2, len(payload), payload)
+
+            # Pack identifier item
+            id = int(random() * 10000)
+            identifierItem = struct.pack('!BHi', 3, 4, id)
+
+            # Pack expiry item
+            tomorrow = int(time.mktime(datetime.now().timetuple()))
+            expiryItem = struct.pack('!BHi', 4, 4, tomorrow)
+
+            # Pack priority item
+            priorityItem = struct.pack('!BHB', 5, 1, 10)
+
+            # Assemble notification packet from items
+            frameData = tokenItem + payloadItem + identifierItem + expiryItem \
+                    + priorityItem
+            packet = struct.pack('!BI' + str(len(frameData)) + 's', 2, len(frameData),
+                    frameData)
+
+            logger.info('Sending push notification...')
+            self._iostream.write(packet)
+
+        def onConnected(future):
+            future.result()
+            _send()
+
+        if self._connected:
+            _send()
+        else:
+            connectFuture = self.connect()
+            IOLoop.current().add_future(connectFuture, onConnected)
+
+
+    def shutdown(self):
+        if self._iostream is not None:
+            self._iostream.close()
+
+        self._connected = False
+
+
+    def _onError(self, response):
+        command, status, notifId = struct.unpack('!BBi', response)
+        logger.error('APNs ERROR: %s %s %s', str(command), str(status), str(notifId))
+
+        # Perform read for error packets
+        self._iostream.read_bytes(6, self._onError)
+
+
+
+    _apnsArgs = None
+    _sslArgs = None
+    _sslSocket = None
+    _iostream = None
+    _connectFuture = None
+    _connected = None
