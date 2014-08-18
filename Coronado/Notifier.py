25b5a05e5ab6b4155e22abe528545fddf3dd01c8
Mukul Majmudar <mukul@curecompanion.com>
2014-08-18 21:15:40 +0000

    Error checking for APNs notifier.

--- Coronado/Notifier.py
+++ Coronado/Notifier.py
@@ -7,6 +7,7 @@ import json
 import time
 from datetime import datetime, timedelta
 import logging
+from Queue import Queue, Empty
 
 import tornado.iostream
 from tornado.iostream import StreamClosedError
@@ -34,11 +35,17 @@ class Notifier(object):
 
 
 class APNsNotifier(Notifier):
+    sentQueue = None
+    ioloop = None
+    timeoutHandles = None
 
-    def __init__(self, apnsArgs, sslArgs):
+    def __init__(self, apnsArgs, sslArgs, ioloop=None):
         self._apnsArgs = apnsArgs
         self._sslArgs = sslArgs
         self._connected = False
+        self.sentQueue = Queue()
+        self.ioloop = ioloop is not None and ioloop or IOLoop.current()
+        self.timeoutHandles = {}
 
 
     def connect(self):
@@ -90,7 +97,12 @@ class APNsNotifier(Notifier):
                     2, len(payload), payload)
 
             # Pack identifier item
-            id = int(random() * 10000)
+            id = None
+            if '_id' not in notification:
+                id = int(random() * 10000)
+                notification['_id'] = id
+            else:
+                id = notification['_id']
             identifierItem = struct.pack('!BHi', 3, 4, id)
 
             # Pack expiry item
@@ -118,9 +130,31 @@ class APNsNotifier(Notifier):
                     def onReconnected(future):
                         future.result()
                         _send()
-                    IOLoop.current().add_future(self._connectFuture, onReconnected)
+                    self.ioloop.add_future(self._connectFuture, onReconnected)
 
-                IOLoop.current().add_timeout(timedelta(seconds=5), retry)
+                self.ioloop.add_timeout(timedelta(seconds=2), retry)
+            else:
+                # Save sent notification in queue. We may have to resend it if
+                # a preceding notification fails.
+                logger.info('Sent notification.')
+                self.sentQueue.put(notification)
+
+                # Pop item after some time
+                def popItem():
+                    logger.info('Popping item from sent queue')
+
+                    # Remove an item from the queue
+                    notifcn = self.sentQueue.get()
+
+                    # Delete its timeout, if any
+                    try:
+                        del self.timeoutHandles[notifcn['_id']]
+                    except KeyError:
+                        pass
+
+                handle = self.ioloop.add_timeout(timedelta(seconds=2), popItem)
+                self.timeoutHandles[notification['_id']] = handle
+
 
         def onConnected(future):
             future.result()
@@ -131,7 +165,7 @@ class APNsNotifier(Notifier):
         else:
             if not self._connecting:
                 self.connect()
-            IOLoop.current().add_future(self._connectFuture, onConnected)
+            self.ioloop.add_future(self._connectFuture, onConnected)
 
 
     def shutdown(self):
@@ -145,8 +179,56 @@ class APNsNotifier(Notifier):
         command, status, notifId = struct.unpack('!BBi', response)
         logger.error('APNs ERROR: %s %s %s', str(command), str(status), str(notifId))
 
-        # Perform read for error packets
-        self._iostream.read_bytes(6, self._onError)
+        # Connection is closed by APNs on error
+        self._connected = False
+
+        # Pop sent-queue until we get the erroneous notification.
+        # Items before the erroneous one were successful
+        poppedNotifcns = []
+        while True:
+            try:
+                notifcn = self.sentQueue.get(False)
+            except Empty:
+                break
+            else:
+                # Remove timeout to pop item from the queue
+                try:
+                    timeoutHandle = self.timeoutHandles[notifId]
+                except KeyError:
+                    pass
+                else:
+                    self.ioloop.remove_timeout(timeoutHandle)
+
+                if notifcn['_id'] == notifId:
+                    break
+
+                poppedNotifcns.append(notifcn)
+
+        # Resend all remaining notifications in the queue
+        resendList = []
+        while True:
+            try:
+                notifcn = self.sentQueue.get(False)
+            except Empty:
+                break
+            else:
+                resendList.append(notifcn)
+
+        if resendList:
+            logger.info('Resending notifications after erroneous one.')
+            for n in resendList:
+                self.send(n)
+        else:
+            logger.info('Resending remaining known notifications after ' + \
+                    'erroneous one.')
+
+            # Resend list was empty, which means that the erroneous notification
+            # was assumed by us to be successful, and so all the popped 
+            # notifications need to be resent. Some notifications may be
+            # permanently lost. This is unavoidable since APNs doesn't
+            # acknowledge successful notifications.
+            for n in poppedNotifcns:
+                self.send(n)
 
 
     _apnsArgs = None
